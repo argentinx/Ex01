e947d48d1e1ee8223bbe0a8cdb1df0826eb4865e
Francesco Argentino <francescoargentino@Mac-di-Francesco.local>
2016-05-26 18:31:51 +0200

    Aggiunta Applicazione a GitHub

--- Ex01/ViewController.swift
+++ Ex01/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ButtonBlu: UIButton!
+    @IBOutlet weak var ButtonRed: UIButton!
+    @IBOutlet weak var LogoBlu: UIImageView!
+    @IBOutlet weak var LogoRed: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlu(sender: AnyObject) {
+        LogoBlu.hidden = true
+    }
 
+    @IBAction func HideRed(sender: AnyObject) {
+        LogoRed.hidden = true
+    }
 }
 
